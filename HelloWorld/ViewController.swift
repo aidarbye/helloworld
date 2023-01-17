7fd029a89b7cae51e5d1f86d4caea9d8e97a4de5
Aidar Nurkin <aidar.nurkin@icloud.com>
2023-01-17 16:28:50 +0600

    Modify, add func

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloworldLabel: UILabel!
+    
+    @IBOutlet var todoButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloworldLabel.isHidden = true
+        todoButton.layer.cornerRadius = 15
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButtonAction() {
+        if helloworldLabel.isHidden {
+            helloworldLabel.isHidden = false
+            todoButton.setTitle("Hide text", for: .normal)
+        } else {
+            helloworldLabel.isHidden = true
+            todoButton.setTitle("Show text", for: .normal)
+        }
+    }
+    
 }
 
